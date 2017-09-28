17cb058cf1edc78bee23d9c3d26669502ac37d46
mulukadan <kadan.md@gmail.com>
2017-09-28 20:55:56 +0300

    Added Scripts.js and other Javascript Files

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+function openFood(evt, foodName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(foodName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+function myFunction() {
+  var Email = $("#myEmail").val();
+  alert("Your Email has been successfully registered as: \n"+Email+"\nThank You !!");
+  $("#myEmail").val("");
+}
